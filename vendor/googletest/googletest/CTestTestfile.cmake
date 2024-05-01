720243960d30f92c81388abdf4f3b3d4242cb9fe
Macintyre Sunde <imacsunde@gmail.com>
2024-05-01 13:07:13 -0400

    directory ignores

--- vendor/googletest/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/imacs/Documents/Code/shader-engine/build/_deps/googletest-src/googletest
-# Build directory: C:/Users/imacs/Documents/Code/shader-engine/vendor/googletest/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
