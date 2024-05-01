257c9c90f3efd78cfaa9dfa111d41d4518f84026
Macintyre Sunde <imacsunde@gmail.com>
2024-05-01 13:02:41 -0400

    readded googletest dir

---
+++ vendor/googletest/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/imacs/Documents/Code/shader-engine/build/_deps/googletest-src
+# Build directory: C:/Users/imacs/Documents/Code/shader-engine/vendor/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("googlemock")
---
+++ vendor/googletest/googletest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/imacs/Documents/Code/shader-engine/build/_deps/googletest-src/googletest
+# Build directory: C:/Users/imacs/Documents/Code/shader-engine/vendor/googletest/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
