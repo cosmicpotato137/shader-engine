d3aac9483c0d5fe6076e17042b0865b9b243e868
Macintyre Sunde <imacsunde@gmail.com>
2024-05-22 00:26:32 -0400

    deploy.bat updates

--- deploy.bat
+++ deploy.bat
@@ -2,5 +2,7 @@
 xcopy /s /e /y "bin\debug" "deploy"
 
 @REM Copy res\shaders\compute\* to deploy folder
-
 xcopy /s /e /y "res\shaders\compute" "deploy\res\shaders\compute"
+
+@REM zip deploy folder
+powershell -Command "Compress-Archive -Path .\deploy -DestinationPath .\ShaderEngine.zip"
